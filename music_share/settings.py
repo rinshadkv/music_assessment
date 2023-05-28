656ca4299d87c995bff56241fb300b546cbbb160
rinshadkv <muhammedrinshadkonu@gmail.com>
2023-05-29 00:38:26 +0530

    database added

--- music_share/settings.py
+++ music_share/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-
+import dj_database_url
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -76,20 +77,17 @@ WSGI_APPLICATION = 'music_share.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
     
-# DATABASES = {
-#     'default': {
+DATABASES = {
+    'default': {
         
-#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME':'music',
-#         'USER':'postgres',
-#         'PASSWORD':'root',
-#         'HOST':'localhost',
-#         'PORT':'5432',
-#     }
+       'ENGINE': 'django.db.backends.sqlite3',
+        'NAME':BASE_DIR/'db.sqlite3',
+        
+    }
     
-# }
+}
 
-
+DATABASES['default']= dj_database_url.config()
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
