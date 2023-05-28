dcff953faf04540bb5edbc8fcb90e1ec89f2f85d
rinshadkv <muhammedrinshadkonu@gmail.com>
2023-05-29 01:26:24 +0530

    database credentials added

--- music_share/settings.py
+++ music_share/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
-from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -80,14 +79,18 @@ WSGI_APPLICATION = 'music_share.wsgi.application'
 DATABASES = {
     'default': {
         
-       'ENGINE': 'django.db.backends.sqlite3',
-        'NAME':BASE_DIR/'db.sqlite3',
+       'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'railway',
+        'USER':'postgres',
+        'PASSWORD':'eInaVryPH5mf62imY8CL',
+        'HOST':'containers-us-west-204.railway.app',
+        'PORT':'7061',
+
         
     }
     
 }
 
-DATABASES['default']= dj_database_url.config()
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
